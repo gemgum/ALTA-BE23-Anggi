2eaaf9d3f7c2e1afd6f624b865038d2396feb3c3
Anggi Eko <anggipamboedi@gmail.com>
2024-05-21 23:18:21 +0700

    problem 3

--- function/problem3/main.go
+++ function/problem3/main.go
@@ -2,27 +2,28 @@ package main
 
 import "fmt"
 
+func tabelPerkalian(angka int) {
+	var spasi = angka
+	var hasil int
+	for i := 1; i < angka; i++ {
+		for j := 1; j < spasi; j++ {
+			hasil = i * j
+
+			fmt.Print(hasil, "\t| ")
+
+		}
+		fmt.Println("")
+
+	}
+}
+
 func main() {
-	//Problem 3 - Bilangan Prima
+	// Problem 3 - Cetak Tabel Perkalian
 	var angka int
-	var isPrima = true
-	fmt.Print("\nCek Bilangan Prima\n\n")
+	fmt.Print("\nCetak Tabel Perkalian\n\n")
 	fmt.Printf("Masukan Angka = ")
 	fmt.Scanln(&angka)
 
-	for i := 2; i < angka; i++ {
-		if angka%i == 0 {
-			isPrima = false
-			break
-		}
-	}
+	tabelPerkalian(angka)
 
-	if angka == 1 {
-		isPrima = false
-	}
-	if isPrima {
-		fmt.Println("Angka", angka, "Merupakan Bilanga Prima")
-	} else {
-		fmt.Println("Angka", angka, "Merupakan Bukan Bilanga Prima")
-	}
 }
